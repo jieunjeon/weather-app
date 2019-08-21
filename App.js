8edb92628d22854018dcf0082fc106ee7b8671de
Jieun Jeon <jieunjeon1023@gmail.com>
2019-08-20 17:23:48 -0700

    add loading screent and get geolocation data from expo-location

--- App.js
+++ App.js
@@ -1,19 +1,33 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {Alert} from "react-native";
+import Loading from "./components/Loading";
+import * as Location from "expo-location";
 
-export default function App() {
+export default class extends React.Component {
+  state = {
+    isLoading: true,
+  }
+  getLocation = async() => {
+    try {
+      await Location.requestPermissionsAsync();
+      const {coords: {latitude, longitude}} = await Location.getCurrentPositionAsync();
+      this.setState({ isLoading: false});
+      // send to API and get weather
+      console.log(latitude, longitude);
+
+
+    } catch (error) {
+      Alert.alert("Can't find you.", "so sad");
+    }
+
+  }
+  componentDidMount() {
+    this.getLocation();
+  }
+  render () {
+    const {isLoading} = this.state;
   return (
-    <View style={styles.container}>
-      <Text>Hello!</Text>
-    </View>
+    isLoading? <Loading /> : <></>
   );
+  }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ components/Loading.js
@@ -0,0 +1,24 @@
+import React from "react";
+import {StyleSheet, Text, View} from "react-native";
+
+export default function Loading() {
+    return (
+        <View style={styles.container}>
+            <Text style={styles.text}>Getting the weather data..</Text>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: "flex-end",
+        paddingHorizontal: 30,
+        paddingVertical: 100,
+        backgroundColor: "#00ABBD"
+    },
+    text: {
+        color: "#fff",
+        fontSize: 35,
+    }
+})
